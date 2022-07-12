de806a7d3da099141585d60b5aaeaa69a32dad4a
Federico Hermida <fhermida@truenorth.co>
2022-07-12 16:06:01 -0300

    Add kubernetes initial configuration

--- apps/bids/src/app.service.ts
+++ apps/bids/src/app.service.ts
@@ -1,8 +1,40 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly httpService: HttpService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World from bids!';
+  }
+
+  async getEventHealth(): Promise<string> {
+    const pingRepository = await lastValueFrom(
+      this.httpService.get<string>(`http://${process.env.EVENT_URL}/`),
+    );
+    return await pingRepository.data;
+  }
+
+  async getQueueHealth(): Promise<string> {
+    const pingRepository = await lastValueFrom(
+      this.httpService.get<string>(`http://${process.env.QUEUE_URL}/`),
+    );
+    return await pingRepository.data;
+  }
+
+  async getRepositoryHealth(): Promise<string> {
+    const pingRepository = await lastValueFrom(
+      this.httpService.get<string>(`http://${process.env.REPOSITORY_URL}/`),
+    );
+    return await pingRepository.data;
+  }
+
+  async getSubscriberHealth(): Promise<string> {
+    const pingRepository = await lastValueFrom(
+      this.httpService.get<string>(`http://${process.env.SUBSCRIBER_URL}/`),
+    );
+    return await pingRepository.data;
   }
 }
