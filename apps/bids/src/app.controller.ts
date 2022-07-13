6d783c349d48e165dd08d0cb238d7e83199fe449
FedericoHermidaTN <98053201+FedericoHermidaTN@users.noreply.github.com>
2022-07-12 23:12:07 -0300

    Feature/Kubernetes environment setup (#1)
    * Add kubernetes initial configuration
    
    * Add new health checks
    
    Co-authored-by: Federico Hermida <fhermida@truenorth.co>

--- apps/bids/src/app.controller.ts
+++ apps/bids/src/app.controller.ts
@@ -42,4 +42,24 @@ export class AppController {
     console.log('PUT /bids/:id');
     console.log({ id, offer });
   }
+
+  @Get('/event-health')
+  async getEventHello(): Promise<string> {
+    return await this.appService.getEventHealth();
+  }
+
+  @Get('/subscriber-health')
+  async getSubscriberHello(): Promise<string> {
+    return await this.appService.getSubscriberHealth();
+  }
+
+  @Get('/queue-health')
+  async getQueueHello(): Promise<string> {
+    return await this.appService.getQueueHealth();
+  }
+
+  @Get('/repository-health')
+  async getRepositoryHello(): Promise<string> {
+    return await this.appService.getRepositoryHealth();
+  }
 }
--- apps/bids/src/app.service.ts
+++ apps/bids/src/app.service.ts
@@ -1,8 +1,40 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly httpService: HttpService) {}
+
   getHello(): string {
-    return 'Hello World From Bids!';
+    return 'Hello World from bids!';
+  }
+
+  async getEventHealth(): Promise<string> {
+    const pingRepository = await lastValueFrom(
+      this.httpService.get<string>(`${process.env.EVENT_URL}`),
+    );
+    return await pingRepository.data;
+  }
+
+  async getQueueHealth(): Promise<string> {
+    const pingRepository = await lastValueFrom(
+      this.httpService.get<string>(`${process.env.QUEUE_URL}`),
+    );
+    return await pingRepository.data;
+  }
+
+  async getRepositoryHealth(): Promise<string> {
+    const pingRepository = await lastValueFrom(
+      this.httpService.get<string>(`${process.env.REPOSITORY_URL}`),
+    );
+    return await pingRepository.data;
+  }
+
+  async getSubscriberHealth(): Promise<string> {
+    const pingRepository = await lastValueFrom(
+      this.httpService.get<string>(`${process.env.SUBSCRIBER_URL}`),
+    );
+    return await pingRepository.data;
   }
 }
