c036ce1a7b73cf60dce811483d7480a42b0e54e2
Guido Cohen Semag <guidocohens@gmail.com>
2022-07-18 11:26:47 -0300

    Add simple and progressive load tests

--- artillery/post-body.js
+++ artillery/post-body.js
@@ -1,23 +1,21 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-var-requires */
 'use strict';
 const fs = require('fs');
 const path = require('path');
 
+const parseJSONAndGetRandomPayload = (file) => {
+  const contents = fs.readFileSync(path.resolve(__dirname, file), 'utf8');
+  const payload = JSON.parse(contents);
+  const randomIndex = Math.floor(Math.random() * payload.length);
+  return payload[randomIndex];
+};
+
 const genPayloads = (userContext, events, done) => {
   try {
-    let bidContents = fs.readFileSync(
-      path.resolve(__dirname, './post-data-bid.json'),
-      'utf8',
-    );
-    const bidPayload = JSON.parse(bidContents);
-    userContext.vars.bidPayload = bidPayload;
-
-    let buyerContents = fs.readFileSync(
-      path.resolve(__dirname, './post-data-buyer.json'),
-      'utf8',
-    );
-    const buyerPayload = JSON.parse(buyerContents);
-    userContext.vars.buyerPayload = buyerPayload;
+    userContext.vars.bidPayload = parseJSONAndGetRandomPayload('./post-data-bid.json');
+    userContext.vars.buyerPayload1 = parseJSONAndGetRandomPayload('./post-data-buyer.json');
+    userContext.vars.buyerPayload2 = parseJSONAndGetRandomPayload('./post-data-buyer.json');
 
     return done();
   } catch (err) {
